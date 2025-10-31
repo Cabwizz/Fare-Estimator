41445a40aedd1c318f0c5f6fe10c9dad6cd26e21
Cabwizz <contact@cabwizz.co.uk>
2025-10-31 02:50:46 +0000

    Update service worker cache version and logic

--- sw.js
+++ sw.js
@@ -1,53 +1,44 @@
-const APP_VERSION = 'v16';
-const CACHE = 'cabwizz-cache-' + APP_VERSION;
+const CACHE = 'cabwizz-cache-v19'; // match v1.9.0
 const ASSETS = [
   './',
   'index.html',
-  'manifest.webmanifest',
-  'sw.js'
+  'sw.js',
+  'manifest.webmanifest'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
   );
-  self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(k => {
-        if (k.startsWith('cabwizz-cache-') && k !== CACHE) {
-          return caches.delete(k);
-        }
-      })
-    ))
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => {
+          if (k !== CACHE) return caches.delete(k);
+        })
+      )
+    )
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
   const req = event.request;
-  event.respondWith(
-    caches.match(req).then(res => {
-      return res || fetch(req).then(networkRes => {
-        return caches.open(CACHE).then(c => {
-          c.put(req, networkRes.clone());
-          return networkRes;
-        });
-      });
-    })
-  );
-});
+  const url = new URL(req.url);
 
-// respond to version requests
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'GET_VERSION') {
-    event.source.postMessage({
-      type: 'VERSION',
-      version: APP_VERSION,
-      cache: CACHE
-    });
+  if (url.origin === location.origin) {
+    event.respondWith(
+      caches.match(req).then((cached) => {
+        if (cached) return cached;
+        return fetch(req).then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE).then((cache) => cache.put(req, copy));
+          return res;
+        }).catch(() => caches.match('index.html'));
+      })
+    );
   }
 });
